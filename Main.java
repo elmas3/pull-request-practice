841dc90c669cb355787dd16f298d17ffc300dde7
Nakamura-Yuhi <57391058+Nakamura-Yuhi@users.noreply.github.com>
2020-05-13 16:33:05 +0900

    適当

--- Main.java
+++ Main.java
@@ -2,7 +2,10 @@ public class Main{
 	public static void main(String[] args ){
 		System.out.println("Hello World");
 		
-		
+		Main main = new Main();
+		Main main2 = main.getMain();
+		main.test();
+		main2.test2();
 	}
 	
 	public Main getMain(){
