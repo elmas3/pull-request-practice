0daf95acb21887969ae6d068a67a07107bb7cbae
Nakamura-Yuhi <57391058+Nakamura-Yuhi@users.noreply.github.com>
2020-05-13 16:23:14 +0900

    Mainインスタンスを返す関数の作成を行ったんこぶ

--- Main.java
+++ Main.java
@@ -1,7 +1,14 @@
 public class Main{
 	public static void main(String[] args ){
 		System.out.println("Hello World");
+		
+		
 	}
+	
+	public Main getMain(){
+		return new Main();
+	}
+	
 	public String test(){
 		return "test";
 	}
